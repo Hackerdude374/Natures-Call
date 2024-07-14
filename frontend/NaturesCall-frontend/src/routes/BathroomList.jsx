8d79f0b78f883d37a385a6e5d3e85cee55c49a5d
Robert Le <robertle982@gmail.com>
2024-07-13 20:12:14 -0400

    /nearby

--- frontend/NaturesCall-frontend/src/routes/BathroomList.jsx
+++ frontend/NaturesCall-frontend/src/routes/BathroomList.jsx
@@ -22,7 +22,7 @@ export default function BathroomList() {
       long,
     }
     try {
-      const response = await fetch(`${API_URL}/nearby`, {
+      const response = await fetch(`$/api/userActions/nearby`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
